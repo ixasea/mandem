922ac48ae2f522557e89b4b72a55e361402e8230
hairlinebrockeb <47501906+hairlinebrockeb@users.noreply.github.com>
2022-08-26 23:19:33 +0100

    Delete loadhub.lua

--- loadhub.lua
+++
@@ -1,4 +0,0 @@
-
-loadstring(game:HttpGet(syn.crypt.base64.decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2hhaXJsaW5lYnJvY2tlYi9tYW5kZW0vbWFpbi9odWIubHVh')))()
---// loads the REAL hub
---obfuscate
